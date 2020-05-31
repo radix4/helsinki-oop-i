fe9baf86976edf859bdbef7952d414cdd90b2d2d
Thang Cao <shiyoline@gmail.com>
2020-05-31 11:18:47 -0700

    completed exercises 3 & 4

--- week1-003.Spruce/src/Spruce.java
+++ week1-003.Spruce/src/Spruce.java
@@ -1,9 +1,18 @@
+/** This program prints a spruce
+ * @author Thang Cao
+ * @date 05/30/2020
+ * */
+
+
 public class Spruce {
 
     public static void main(String[] args) {
-
-        // Write code here
-        System.out.println("*");
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+        System.out.println("    *");
     }
 
 }
