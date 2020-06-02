e21e69515b24d9f88ecfe62059283b1258452dc7
Thang Cao <shiyoline@gmail.com>
2020-06-02 11:55:22 -0700

    exercise 7 completed

--- week1-007.Multiplication/src/Multiplication.java
+++ week1-007.Multiplication/src/Multiplication.java
@@ -1,3 +1,8 @@
+/** This program multiplies the two numbers a & b
+ * @author Thang Cao
+ * @date 06/02/2020
+ * */
+
 public class Multiplication {
 
     public static void main(String[] args) {
@@ -5,7 +10,7 @@ public class Multiplication {
         int a = 1337;
         int b = 42;
 
-        // Program your solution here. Remember to use variables a and b!
+        System.out.println(a + " * " + b + " = " + (a*b));
     }
 
 }
