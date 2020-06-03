1f079c87d766ccaca5a942ab687e06695162813c
Thang Cao <shiyoline@gmail.com>
2020-06-03 12:40:53 -0700

    exercise 10 completed

--- week1-010.CircleCircumference/src/Circumference.java
+++ week1-010.CircleCircumference/src/Circumference.java
@@ -1,10 +1,20 @@
-
+/** This program calculates the circumferience of the circle
+ * @author Thang Cao
+ * @date 06/03/2020
+ * */
 import java.util.Scanner;
 
 public class Circumference {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // Program your solution here 
+        System.out.print("Type the radius: ");
+
+        double radius = Double.parseDouble(reader.nextLine());
+        double pi = Math.PI;
+        double circumference = 2 * pi * radius;
+
+        System.out.println();
+        System.out.println("Circumference of the circle: " + circumference);
     }
 }
