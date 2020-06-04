66fc7113421c2999cb0f136fc8cb213b8626e245
Thang Cao <shiyoline@gmail.com>
2020-06-04 13:04:16 -0700

    exercise 11 completed

--- week1-011.BiggerNumber/src/BiggerNumber.java
+++ week1-011.BiggerNumber/src/BiggerNumber.java
@@ -1,3 +1,8 @@
+/** This program asks the user for 2 ints and prints the larger
+ * @author Thang Cao
+ * @date 06/04/2020
+ * */
+
 
 import java.util.Scanner;
 
@@ -6,6 +11,12 @@ public class BiggerNumber {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // Implement your program here. Remember to ask the input from user
+        System.out.print("Type a number: ");
+        int num1 = Integer.parseInt(reader.nextLine());
+        System.out.print("Type another number: ");
+        int num2 = Integer.parseInt(reader.nextLine());
+
+        System.out.println("The bigger number of the two numbers given was: "
+                + Math.max(num1,num2));
     }
 }
