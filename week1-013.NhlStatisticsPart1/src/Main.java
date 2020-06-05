fffa8b2f4e8824a85a881aa50274425338ca4e1a
Thang Cao <shiyoline@gmail.com>
2020-06-05 11:18:30 -0700

    exercise 13 completed

--- week1-013.NhlStatisticsPart1/src/Main.java
+++ week1-013.NhlStatisticsPart1/src/Main.java
@@ -1,8 +1,29 @@
 
 import nhlstats.NHLStatistics;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Top ten by points");
+        NHLStatistics.sortByGoals();
+        NHLStatistics.top(10);
+
+        System.out.println("25 by penalties");
+        NHLStatistics.sortByPenalties();
+        NHLStatistics.top(25);
+
+        System.out.println("Sidney Crosby stats");
+        NHLStatistics.searchByPlayer("Sidney Crosby");
+
+        System.out.println("Stats for Philadelphia Flyers (PHI)");
+        NHLStatistics.teamStatistics("PHI");
+
+        System.out.println("Players in Anaheim Ducks (ANA)");
+        NHLStatistics.sortByPoints();
+        NHLStatistics.teamStatistics("ANA");
     }
 }
