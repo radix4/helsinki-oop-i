de6525cbe9f4994c14941583c80e9cc6fd123f85
Thang Cao <shiyoline@gmail.com>
2020-06-06 10:51:29 -0700

    Exercise 15 completed

--- week1-015.AgeOfMajority/src/AgeOfMajority.java
+++ week1-015.AgeOfMajority/src/AgeOfMajority.java
@@ -1,4 +1,8 @@
-
+/** This program asks for the user's age and tells whether the user has reached
+ * the age of majority (i.e. 18 years old or older)
+ * @author  Thang Cao
+ * @since   06/06/2020
+ * */
 import java.util.Scanner;
 
 public class AgeOfMajority {
@@ -6,6 +10,13 @@ public class AgeOfMajority {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // Type your program here 
+        System.out.print("How old are you? ");
+        int age = Integer.parseInt(reader.nextLine());
+
+        if (age >= 18) {
+            System.out.println("\nYou have reached the age of majority!");
+        } else {
+            System.out.println("\nYou have not reached the age of majority yet!");
+        }
     }
 }
