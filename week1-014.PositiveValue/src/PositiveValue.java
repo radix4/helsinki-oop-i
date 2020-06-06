787e3c75554e8382b72d81a2500bbb75ecc5790e
Thang Cao <shiyoline@gmail.com>
2020-06-06 10:45:59 -0700

    Exercise 14 completed

--- week1-014.PositiveValue/src/PositiveValue.java
+++ week1-014.PositiveValue/src/PositiveValue.java
@@ -1,4 +1,7 @@
-
+/** This program tells if a number is positive
+ * @author  Thang Cao
+ * @since   06/05/2020
+ * */
 import java.util.Scanner;
 
 public class PositiveValue {
@@ -6,6 +9,14 @@ public class PositiveValue {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // Type your program here:
+        System.out.print("Type a number: ");
+        int num = Integer.parseInt(reader.nextLine());
+        System.out.println();
+
+        if (num > 0) {
+            System.out.println("The number is a positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
     }
 }
