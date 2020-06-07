048b326183ab64ca3488f3021fa8a3acc901d1ae
Thang Cao <shiyoline@gmail.com>
2020-06-07 11:44:00 -0700

    Exercise 17 completed

--- week1-017.GreaterNumber/src/GreaterNumber.java
+++ week1-017.GreaterNumber/src/GreaterNumber.java
@@ -10,5 +10,18 @@ public class GreaterNumber {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.print("Type the first number: ");
+        int num1 = Integer.parseInt(reader.nextLine());
+
+        System.out.print("Type the second number: ");
+        int num2 = Integer.parseInt(reader.nextLine());
+
+        System.out.println();
+
+        if (num1 == num2){
+            System.out.println("The numbers are equal!");
+        } else {
+            System.out.println("Greater number: " + Math.max(num1, num2));
+        }
     }
 }
