e161c942324f806138808f67ccb89dbe5b2f3edc
Thang Cao <shiyoline@gmail.com>
2020-06-08 12:46:25 -0700

    Exercise 19 completed.

--- week1-019.AgeCheck/src/AgeCheck.java
+++ week1-019.AgeCheck/src/AgeCheck.java
@@ -1,4 +1,7 @@
-
+/** This program checks if someone's age is reasonable (between 0 and 120)
+ * @author  Thang Cao
+ * @since   06/08/2020
+ * */
 import java.util.Scanner;
 
 public class AgeCheck {
@@ -6,5 +9,13 @@ public class AgeCheck {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.print("How old are you? ");
+        int age = Integer.parseInt(reader.nextLine());
+
+        if (age >= 0 && age <= 120){
+            System.out.println("OK");
+        } else {
+            System.out.println("Impossible!");
+        }
     }
 }
