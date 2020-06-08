9ee1885c0b64152b20853f1a803d029ebf9b106a
Thang Cao <shiyoline@gmail.com>
2020-06-08 12:39:00 -0700

    Exercise 18 completed

--- week1-018.GradesAndPoints/src/GradesAndPoints.java
+++ week1-018.GradesAndPoints/src/GradesAndPoints.java
@@ -1,4 +1,7 @@
-
+/** This program gives course grade based on points [0-60]
+ * @author  Thang Cao
+ * @since   06/08/2020
+ * */
 import java.util.Scanner;
 
 public class GradesAndPoints {
@@ -6,5 +9,21 @@ public class GradesAndPoints {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.print("Type the points [0-60]: ");
+        int point = Integer.parseInt(reader.nextLine());
+
+        if (point <= 60 && point >= 50){
+            System.out.println("Grade: 5");
+        } else if (point < 50 && point >= 45){
+            System.out.println("Grade: 4");
+        }else if (point < 45 && point >= 40){
+            System.out.println("Grade: 3");
+        }else if (point < 40 && point >= 35){
+            System.out.println("Grade: 2");
+        }else if (point < 35 && point >= 30){
+            System.out.println("Grade: 1");
+        } else {
+            System.out.println("Grade: failed");
+        }
     }
 }
