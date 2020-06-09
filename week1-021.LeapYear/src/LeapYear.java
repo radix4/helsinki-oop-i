a26237e5a8e5a92b1b37c95702bca2afeadc0a6a
Thang Cao <shiyoline@gmail.com>
2020-06-09 09:04:16 -0700

    Exercise 21 completed.

--- week1-021.LeapYear/src/LeapYear.java
+++ week1-021.LeapYear/src/LeapYear.java
@@ -1,3 +1,8 @@
+/** This program asks for a year and tells whether it is a leap year
+ * @author  Thang Cao
+ * @since   06/09/2020
+ * */
+
 
 import java.util.Scanner;
 
@@ -6,5 +11,13 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.print("Type a year: ");
+        int year = Integer.parseInt(reader.nextLine());
+
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)){
+            System.out.println("The year is a leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
     }
 }
