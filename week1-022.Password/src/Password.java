c423be5f5e9da51ac5629a65e116c19015fff6d5
Thang Cao <shiyoline@gmail.com>
2020-06-10 13:13:36 -0700

    Exercise 22 completed.

--- week1-022.Password/src/Password.java
+++ week1-022.Password/src/Password.java
@@ -1,4 +1,7 @@
-
+/** This program asks for the user's password, reveal secret if password is right.
+ * @author  Thang Cao
+ * @since   06/10/2020
+ * */
 import java.util.Scanner;
 
 public class Password {
@@ -7,6 +10,18 @@ public class Password {
         Scanner reader = new Scanner(System.in);
         String password = "carrot"; // Use carrot as password when running tests.
 
-        // Write your code here
+        while (true){
+            System.out.print("Type the password: ");
+            String compare = reader.nextLine();
+
+            if (compare.equals(password)){
+                System.out.println("Right!\n");
+                System.out.println("The secret is: jryy qbar!");
+                break;
+            }
+
+            System.out.println("Wrong!");
+
+        }
     }
 }
