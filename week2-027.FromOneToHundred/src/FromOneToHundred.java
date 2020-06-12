c07678c44568ef426f7074880e4a0754d9c0c5b4
Thang Cao <shiyoline@gmail.com>
2020-06-12 12:04:42 -0700

    Exercise 27 completed.

--- week2-027.FromOneToHundred/src/FromOneToHundred.java
+++ week2-027.FromOneToHundred/src/FromOneToHundred.java
@@ -1,7 +1,12 @@
-
+/** This program prints integers from 1 to 100.
+ * @author  Thang Cao
+ * @since   06/12/2020
+ * */
 public class FromOneToHundred {
 
     public static void main(String[] args) {
-        // Write your program here
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
     }
 }
