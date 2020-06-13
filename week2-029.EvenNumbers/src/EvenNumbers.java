e88acf5252e35b11b0d50ebc6f420d2a53d047d4
Thang Cao <shiyoline@gmail.com>
2020-06-13 10:47:42 -0700

    Exercise 29 completed.

--- week2-029.EvenNumbers/src/EvenNumbers.java
+++ week2-029.EvenNumbers/src/EvenNumbers.java
@@ -1,7 +1,12 @@
-
+/** This program prints even numbers between 2 and 100.
+ * @author  Thang Cao
+ * @since   06/13/2020
+ * */
 public class EvenNumbers {
 
     public static void main(String[] args) {
-        // Write your code here
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(i * 2);
+        }
     }
 }
