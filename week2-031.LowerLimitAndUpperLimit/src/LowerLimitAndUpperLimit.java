0e62b9430622c26b7317327eae36d29db45797dd
Thang Cao <shiyoline@gmail.com>
2020-06-14 11:43:04 -0700

    Exercise 31 completed.

--- week2-031.LowerLimitAndUpperLimit/src/LowerLimitAndUpperLimit.java
+++ week2-031.LowerLimitAndUpperLimit/src/LowerLimitAndUpperLimit.java
@@ -1,3 +1,9 @@
+/**
+ * This program asks user for first number and last number
+ * then prints all in between.
+ * @author  Thang Cao
+ * @since   06/14/2020
+ * */
 
 import java.util.Scanner;
 
@@ -6,7 +12,13 @@ public class LowerLimitAndUpperLimit {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // write your code here
+        System.out.print("First: ");
+        int first = Integer.parseInt(reader.nextLine());
+        System.out.print("Last: ");
+        int last = Integer.parseInt(reader.nextLine());
 
+        for (int i = first; i <= last; i++) {
+            System.out.println(i);
+        }
     }
 }
