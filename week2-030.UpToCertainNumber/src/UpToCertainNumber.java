7780c46cbdda40168057cc46bc406db11bb246e7
Thang Cao <shiyoline@gmail.com>
2020-06-14 11:37:53 -0700

    Exercise 30 completed.

--- week2-030.UpToCertainNumber/src/UpToCertainNumber.java
+++ week2-030.UpToCertainNumber/src/UpToCertainNumber.java
@@ -1,3 +1,8 @@
+/**
+ * This program prints whole numbers from 1 to the number user enters.
+ * @author  Thang Cao
+ * @since   06/14/2020
+ * */
 
 import java.util.Scanner;
 
@@ -6,8 +11,13 @@ public class UpToCertainNumber {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        
-        // Write your code here
+
+        System.out.print("Up to what number? ");
+        int upperBound = Integer.parseInt(reader.nextLine());
+
+        for (int i = 1; i <= upperBound; i++) {
+            System.out.println(i);
+        }
         
     }
 }
