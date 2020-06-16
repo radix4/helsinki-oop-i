978282550de127515b0f84f2033dc045eaad7904
Thang Cao <shiyoline@gmail.com>
2020-06-15 23:01:39 -0700

    Exercise 33 completed.

--- week2-033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
+++ week2-033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
@@ -1,3 +1,8 @@
+/**
+ * This program calculates the sum from lower to upper bound.
+ * @author  Thang Cao
+ * @since   06/14/2020
+ * */
 
 import java.util.Scanner;
 
@@ -5,6 +10,16 @@ import java.util.Scanner;
 public class TheSumBetweenTwoNumbers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        
+        System.out.print("First: ");
+        int lowerBound = Integer.parseInt(reader.nextLine());
+        System.out.print("Last: ");
+        int upperBound = Integer.parseInt(reader.nextLine());
+        int sum = 0;
+
+        for (int i = lowerBound; i <= upperBound; i++) {
+            sum += i;
+        }
+
+        System.out.println("The sum is " + sum);
     }
 }
