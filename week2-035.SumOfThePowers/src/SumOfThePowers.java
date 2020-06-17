c4e37c41606a807043f7fcb772c47a2343eeacf3
Thang Cao <shiyoline@gmail.com>
2020-06-16 21:51:53 -0700

    Exercise 35 completed.

--- week2-035.SumOfThePowers/src/SumOfThePowers.java
+++ week2-035.SumOfThePowers/src/SumOfThePowers.java
@@ -1,10 +1,25 @@
+/**
+ * This program calculates the sum of the powers, with base = 2.
+ * @author  Thang Cao
+ * @since   06/16/2020
+ * */
 
 import java.util.Scanner;
 
 public class SumOfThePowers {
 
+    /** 2^4 = 2 * 2 * 2 * 2. */
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.print("Type a number: ");
+        int n = Integer.parseInt(reader.nextLine());
+        int sum = 0;
+
+        for (int i = 0; i <= n; i++) {
+            sum += (int)Math.pow(2,i);
+        }
+
+        System.out.println("The result is " + sum);
     }
 }
