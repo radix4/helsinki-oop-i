7b8fb23c45368dc1ee8e728e1e1e95c032610f2c
Thang Cao <shiyoline@gmail.com>
2020-06-17 21:35:54 -0700

    Exercise 36 completed.

--- week2-036.LoopsEndingRemembering/src/LoopsEndingRemembering.java
+++ week2-036.LoopsEndingRemembering/src/LoopsEndingRemembering.java
@@ -16,6 +16,8 @@ public class LoopsEndingRemembering {
 
         int sum = 0;
         int count = 0;
+        int evenCount = 0;
+        int oddCount = 0;
 
         while (true) {
             int n = Integer.parseInt(reader.nextLine());
@@ -27,6 +29,12 @@ public class LoopsEndingRemembering {
             sum += n;
             count++;
 
+            if (n % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+
         }
 
         double average = (double) sum / count;
@@ -35,5 +43,8 @@ public class LoopsEndingRemembering {
         System.out.println("The sum is " + sum);
         System.out.println("How many numbers: " + count);
         System.out.println("Average: " + average);
+        System.out.println("Even numbers: " + evenCount);
+        System.out.println("Odd numbers: " + oddCount);
+
     }
 }
