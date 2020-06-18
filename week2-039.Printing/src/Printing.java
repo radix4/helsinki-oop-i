7b9b7e73d8261ca62ec5c13bfb70fbf1f2bf30e1
Thang Cao <shiyoline@gmail.com>
2020-06-18 09:18:34 -0700

    Exercise 39.2 completed.

--- week2-039.Printing/src/Printing.java
+++ week2-039.Printing/src/Printing.java
@@ -23,7 +23,12 @@ public class Printing {
     }
 
     public static void printRectangle(int width, int height) {
-        // 39.3
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     public static void printTriangle(int size) {
