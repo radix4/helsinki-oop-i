a909b59c19049fe52379a4e7aa7fe219f5036d14
Thang Cao <shiyoline@gmail.com>
2020-06-17 21:31:35 -0700

    Exercise 36.3 completed.

--- week2-036.LoopsEndingRemembering/src/LoopsEndingRemembering.java
+++ week2-036.LoopsEndingRemembering/src/LoopsEndingRemembering.java
@@ -15,6 +15,7 @@ public class LoopsEndingRemembering {
         System.out.println("Type numbers:");
 
         int sum = 0;
+        int count = 0;
         while (true) {
             int n = Integer.parseInt(reader.nextLine());
 
@@ -23,9 +24,11 @@ public class LoopsEndingRemembering {
             }
 
             sum += n;
+            count++;
         }
 
         System.out.println("Thank you and see you later!");
         System.out.println("The sum is " + sum);
+        System.out.println("How many numbers: " + count);
     }
 }
