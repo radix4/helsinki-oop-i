371feb4b204df1aa2bc2bce4ffa8eb94c08620d4
Thang Cao <shiyoline@gmail.com>
2020-06-17 21:29:45 -0700

    Exercise 36.1 completed.

--- week2-036.LoopsEndingRemembering/src/LoopsEndingRemembering.java
+++ week2-036.LoopsEndingRemembering/src/LoopsEndingRemembering.java
@@ -1,11 +1,26 @@
+
+
+/**
+ * This program demonstrates the uses of different features of a loop.
+ * @author  Thang Cao
+ * @since   06/14/2020
+ * */
+
 import java.util.Scanner;
 
 public class LoopsEndingRemembering {
     public static void main(String[] args) {
-        // program in this project exercises 36.1-36.5
-        // actually this is just one program that is split in many parts
-        
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Type numbers:");
+        while (true) {
+            int n = Integer.parseInt(reader.nextLine());
+
+            if (n == -1) {
+                break;
+            }
+        }
+
+        System.out.println("Thank you and see you later!");
     }
 }
