b678536564373e70d816cd0e01e82e2dee86cdbb
Thang Cao <shiyoline@gmail.com>
2020-06-17 21:39:38 -0700

    Exercise 37 completed.

--- week2-037.PrintingOutText/src/PrintingOutText.java
+++ week2-037.PrintingOutText/src/PrintingOutText.java
@@ -1,7 +1,14 @@
+
+/**
+ * This program prints a text using a separate method.
+ * @author  Thang Cao
+ * @since   06/17/2020
+ * */
+
 public class PrintingOutText {
 
     public static void printText() {
-        // Write your code here
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
     }
 
     public static void main(String[] args) {
