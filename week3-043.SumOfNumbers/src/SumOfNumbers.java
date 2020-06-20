987062b83a3f27bf8b42f4b201fb410180b785b4
Thang Cao <shiyoline@gmail.com>
2020-06-20 13:04:55 -0700

    Exercise 43 completed.

--- week3-043.SumOfNumbers/src/SumOfNumbers.java
+++ week3-043.SumOfNumbers/src/SumOfNumbers.java
@@ -1,8 +1,14 @@
+/**
+ * This program calls a method that returns the sum of all the parameters.
+ * @author  Thang Cao
+ * @since   06/20/2020
+ * */
+
+
 public class SumOfNumbers {
 
     public static int sum(int number1, int number2, int number3, int number4) {
-        // write your code here
-        return -1;
+        return number1 + number2 + number3 + number4;
     }
 
     public static void main(String[] args) {
