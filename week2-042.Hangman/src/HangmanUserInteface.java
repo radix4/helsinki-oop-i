eecded70320d85f466f08509c22cdebd2f3cbf2f
Thang Cao <shiyoline@gmail.com>
2020-06-20 13:03:11 -0700

    Exercise 42 completed.

--- week2-042.Hangman/src/HangmanUserInteface.java
+++ week2-042.Hangman/src/HangmanUserInteface.java
@@ -1,3 +1,8 @@
+/**
+ * This program represents a text based UI for the Hangman game.
+ * @author  Thang Cao
+ * @since   06/20/2020
+ * */
 
 import hangman.Hangman;
 import java.util.Scanner;
@@ -15,7 +20,24 @@ public class HangmanUserInteface {
         printMenu();
         System.out.println("");
 
-        // PROGRAM YOUR SOLUTION HERE
+        while (hangman.gameOn()) {
+            System.out.println("");
+            System.out.println("Type a command: ");
+            String command = reader.nextLine();
+
+            if (command.equals("quit")) {
+                break;
+            } else if (command.equals("status")) {
+                hangman.printStatus();
+            } else if (command.length() == 1) {
+                hangman.guess(command);
+            } else if (command.isEmpty()) {
+                printMenu();
+            }
+
+            hangman.printMan();
+            hangman.printWord();
+        }
 
 
         System.out.println("Thank you for playing!");
