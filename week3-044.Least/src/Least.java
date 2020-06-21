0b49803fdbdc0d5ad29372eac2d6fc509b0a1108
Thang Cao <shiyoline@gmail.com>
2020-06-21 15:37:29 -0700

    Exercise 44 completed.

--- week3-044.Least/src/Least.java
+++ week3-044.Least/src/Least.java
@@ -1,11 +1,13 @@
+/**
+ * This program returns the smaller number given 2 parameters.
+ * @author  Thang Cao
+ * @since   06/21/2020
+ * */
 
 public class Least {
 
     public static int least(int number1, int number2) {
-        // write program code here
-        // do not print anything inside the method
-        // method needs a return in the end
-        return -1;
+        return Math.min(number1,number2);
     }
 
     public static void main(String[] args) {
