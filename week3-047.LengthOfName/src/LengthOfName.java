4e444e06f16155176b401bc798e4b02472debed5
Thang Cao <shiyoline@gmail.com>
2020-06-22 13:38:32 -0700

    Exercise 47 completed.

--- week3-047.LengthOfName/src/LengthOfName.java
+++ week3-047.LengthOfName/src/LengthOfName.java
@@ -1,13 +1,22 @@
+/**
+ * This program returns the length of a name.
+ * @author  Thang Cao
+ * @since   06/22/2020
+ * */
 
 import java.util.Scanner;
 
 public class LengthOfName {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // call your method from here
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+
+        System.out.println("Number of characters: " + calculateCharacters(name));
     }
     
-    // do here the method
-    // public static int calculateCharacters(String text)
+    public static int calculateCharacters(String text) {
+        return text.length();
+    }
     
 }
