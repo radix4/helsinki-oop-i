60c9deea94d290ad7f52959e4ed394804b547f5e
Thang Cao <shiyoline@gmail.com>
2020-06-23 08:22:47 -0700

    Exercise 48 completed.

--- week3-048.FirstCharacter/src/FirstCharacter.java
+++ week3-048.FirstCharacter/src/FirstCharacter.java
@@ -1,8 +1,21 @@
+/**
+ * This program asks for the user's name and gives the first character.
+ * @author  Thang Cao
+ * @since   06/23/2020
+ * */
+
 import java.util.Scanner;
 
 public class FirstCharacter {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        System.out.println("First character: " + firstCharacter(name));
+    }
+
+    public static char firstCharacter(String text) {
+        return text.charAt(0);
     }
 }
