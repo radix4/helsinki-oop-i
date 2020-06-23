9a95a7af3a8e69d10df60731089c34ed9f43fd95
Thang Cao <shiyoline@gmail.com>
2020-06-23 08:25:50 -0700

    Exercise 49 completed.

--- week3-049.LastCharacter/src/LastCharacter.java
+++ week3-049.LastCharacter/src/LastCharacter.java
@@ -1,3 +1,9 @@
+/**
+ * This program asks for the user's name and prints the last character.
+ * @author  Thang Cao
+ * @since   06/23/2020
+ * */
+
 import java.util.Scanner;
 
 
@@ -6,5 +12,14 @@ public class LastCharacter {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.print("Type your name: ");
+
+        String name = reader.nextLine();
+        System.out.println("Last character: " + lastCharacter(name));
     }
+
+    public static char lastCharacter(String text){
+        return text.charAt(text.length()-1);
+    }
+
 }
