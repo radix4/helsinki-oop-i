031b7eff2768146913544860e0aec211cb6fd63c
Thang Cao <shiyoline@gmail.com>
2020-06-24 08:41:01 -0700

    Exercise 51 completed.

--- week3-051.SeparatingCharacters/src/SeparatingCharacters.java
+++ week3-051.SeparatingCharacters/src/SeparatingCharacters.java
@@ -1,3 +1,8 @@
+/**
+ * This program separates the user's name characters.
+ * @author  Thang Cao
+ * @since   06/24/2020
+ * */
 
 import java.util.Scanner;
 
@@ -5,5 +10,14 @@ public class SeparatingCharacters {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+
+
+            for (int i = 0; i < name.length(); i++) {
+                System.out.println((i + 1) + ". character: " + name.charAt(i));
+            }
+
     }
 }
