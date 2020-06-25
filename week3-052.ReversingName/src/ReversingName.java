38ede87955463005d7fad03bb4379cdd0f388bfc
Thang Cao <shiyoline@gmail.com>
2020-06-25 08:25:32 -0700

    Exercise 52 completed.

--- week3-052.ReversingName/src/ReversingName.java
+++ week3-052.ReversingName/src/ReversingName.java
@@ -1,7 +1,18 @@
+/**
+ * This program asks for the user's name and prints it in reverse.
+ * @author  Thang Cao
+ * @since   06/25/2020
+ * */
+
 import java.util.Scanner;
 
 public class ReversingName {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type your name: ");
+        StringBuilder name = new StringBuilder(reader.nextLine());
+
+        System.out.println("In reverse order: " + name.reverse());
     }
 }
