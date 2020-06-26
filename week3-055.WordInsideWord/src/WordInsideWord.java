e05907ef41b461da64914ec2c1721df61c9c21a9
Thang Cao <shiyoline@gmail.com>
2020-06-26 09:16:44 -0700

    Exercise 55 completed.

--- week3-055.WordInsideWord/src/WordInsideWord.java
+++ week3-055.WordInsideWord/src/WordInsideWord.java
@@ -1,9 +1,25 @@
-
+/**
+ * This program checks if the 2nd word is in the first word.
+ * @author  Thang Cao
+ * @since   06/26/2020
+ * */
 import java.util.Scanner;
 
 public class WordInsideWord {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.print("Type the first word: ");
+        String first = reader.nextLine();
+        System.out.print("Type the second word: ");
+        String second = reader.nextLine();
+
+        if (first.contains(second)) {
+            System.out.println("The word '" + second + "' is found in the word '" +
+                    first + "'.");
+        } else {
+            System.out.println("The word '" + second + "' is not found in the word '" +
+                    first + "'.");
+        }
     }
 }
