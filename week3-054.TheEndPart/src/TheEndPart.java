ebd2af0282371395e43e661f7af4c1bc8a9f9dae
Thang Cao <shiyoline@gmail.com>
2020-06-26 09:12:41 -0700

    Exercise 54 completed.

--- week3-054.TheEndPart/src/TheEndPart.java
+++ week3-054.TheEndPart/src/TheEndPart.java
@@ -1,7 +1,20 @@
+/**
+ * This program prints the end of a word.
+ * @author  Thang Cao
+ * @since   06/26/2020
+ * */
 import java.util.Scanner;
 
 public class TheEndPart {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type a word: ");
+        String word = reader.nextLine();
+        System.out.print("Length of the end part: ");
+        int length = Integer.parseInt(reader.nextLine());
+        int lastIndex = word.length();
+
+        System.out.print("Result: " + word.substring(lastIndex - length, lastIndex));
     }
 }
