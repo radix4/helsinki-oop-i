045641c7c629114907e9ca748f06851bf890213c
Thang Cao <shiyoline@gmail.com>
2020-06-27 09:00:22 -0700

    Exercise 57 completed.

--- week3-057.Words/src/Words.java
+++ week3-057.Words/src/Words.java
@@ -1,3 +1,9 @@
+/**
+ * This program demonstrates the functions of a ArrayList data structure.
+ * @author  Thang Cao
+ * @since   06/27/2020
+ * */
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -5,5 +11,19 @@ public class Words {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         ArrayList<String> words = new ArrayList<String>();
+
+        while (true) {
+            System.out.print("Type a word: ");
+            String word = reader.nextLine();
+            if (word.isEmpty()) {
+                break;
+            }
+
+            words.add(word);
+        }
+
+        for (String w : words) {
+            System.out.println(w);
+        }
     }
 }
