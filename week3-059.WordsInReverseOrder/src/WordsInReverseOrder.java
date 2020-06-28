f6b4118459353cfd9db8226ac681f6914f98088f
Thang Cao <shiyoline@gmail.com>
2020-06-28 12:24:39 -0700

    Exercise 59 completed.

--- week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
+++ week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
@@ -1,3 +1,9 @@
+/**
+ * This program prints words in reverse order of an arraylist.
+ * @author  Thang Cao
+ * @since   06/28/2020
+ * */
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -5,6 +11,23 @@ import java.util.Scanner;
 public class WordsInReverseOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // create here an ArrayList
+        ArrayList<String> list = new ArrayList<>();
+
+        while(true) {
+            System.out.print("Type a word: ");
+            String word = reader.nextLine();
+
+            if (word.isEmpty()) {
+                break;
+            }
+
+            list.add(word);
+        }
+
+        Collections.reverse(list);
+
+        for (String s : list) {
+            System.out.println(s);
+        }
     }
 }
