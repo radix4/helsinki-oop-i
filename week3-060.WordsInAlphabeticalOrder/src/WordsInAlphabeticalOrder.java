e1d56f8efc242845b48edf8b365ab68cf2afed7f
Thang Cao <shiyoline@gmail.com>
2020-06-29 11:01:53 -0700

    Exercise 60 completed.

--- week3-060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
+++ week3-060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
@@ -1,3 +1,8 @@
+/**
+ * This program print words in alphabetical order.
+ * @author  Thang Cao
+ * @since   06/29/2020
+ * */
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,6 +12,25 @@ public class WordsInAlphabeticalOrder {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // create here an ArrayList
+        ArrayList<String> words = new ArrayList<>();
+
+        while (true) {
+            System.out.print("Type a word: ");
+            String word = reader.nextLine();
+
+            if (word.isEmpty()) {
+                break;
+            }
+
+            words.add(word);
+        }
+        Collections.sort(words);
+        print(words);
+    }
+
+    public static void print(ArrayList<String> words) {
+        for (String w : words) {
+            System.out.println(w);
+        }
     }
 }
