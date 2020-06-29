467e4866b5664b043e53a32e5026208fa4d4bdaa
Thang Cao <shiyoline@gmail.com>
2020-06-29 11:12:54 -0700

    Exercise 61 completed.

--- week3-061.NumberOfItems/src/NumberOfItems.java
+++ week3-061.NumberOfItems/src/NumberOfItems.java
@@ -1,8 +1,17 @@
+/**
+ * This program returns the number of items in a list.
+ * @author  Thang Cao
+ * @since   06/29/2020
+ * */
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class NumberOfItems {
 
-    // implement here the method countItems
+    public static int countItems(ArrayList<String> list){
+        return list.size();
+    }
 
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<String>();
@@ -11,7 +20,7 @@ public class NumberOfItems {
         list.add("Hello");
         System.out.println("There are this many items on the list:");
         // You can remove the comment from below when the method is done
-        //System.out.println(countItems(list)); 
+         System.out.println(countItems(list));
     }
 
 }
