82fdb804a519335b75beba882929b177d27e993b
Thang Cao <shiyoline@gmail.com>
2020-07-03 19:11:43 -0700

    Exercise 69 coompleted.

--- week3-069.Palindrome/src/Palindromi.java
+++ week3-069.Palindrome/src/Palindromi.java
@@ -1,10 +1,22 @@
+/**
+ * This program checks if a string is a palindrome (reads the same forward and backward)
+ * @author  Thang Cao
+ * @since   07/03/2020
+ * */
+
 import java.util.Scanner;
 
 public class Palindromi {
 
+    public static String reverse(String text){
+        StringBuilder reverse = new StringBuilder(text);
+        reverse.reverse();
+
+        return reverse.toString();
+    }
+
     public static boolean palindrome(String text) {
-        // write code here
-        return false;
+        return text.equals(reverse(text));
     }
 
     public static void main(String[] args) {
