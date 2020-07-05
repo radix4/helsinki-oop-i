a9f575b3aa761a2e4e7eb1c2d5837b50681cc150
Thang Cao <shiyoline@gmail.com>
2020-07-04 22:00:45 -0700

    Exercise 71 completed.

--- week3-071.SmartCombining/src/SmartCombining.java
+++ week3-071.SmartCombining/src/SmartCombining.java
@@ -1,7 +1,19 @@
+/**
+ * This program combines two lists into one, without repeat.
+ * @author  Thang Cao
+ * @since   07/04/2020
+ * */
+
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class SmartCombining {
+    public static void smartCombine(ArrayList<Integer> one, ArrayList<Integer> two) {
+        ArrayList<Integer> twoCopy = new ArrayList<>(two); // temp arrayList
+        twoCopy.removeAll(one);  // remove duplicates
+        one.addAll(twoCopy);
+    }
+
     public static void main(String[] args) {
 
         ArrayList<Integer> list1 = new ArrayList<Integer>();
@@ -12,7 +24,7 @@ public class SmartCombining {
         Collections.addAll(list2, 5, 10, 4, 3, 7);
 
         // remove comment when method ready
-        //smartCombine(list1, list2);
+        smartCombine(list1, list2);
         System.out.println(list1);
         System.out.println(list2);
     }
