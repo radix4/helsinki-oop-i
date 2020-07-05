1b4ed7051f24c9a358b0594d8cdabb3a278020d1
Thang Cao <shiyoline@gmail.com>
2020-07-05 12:22:27 -0700

    Exercise 72.2 completed.

--- week4-072.1.YourFirstAccount/src/Accounts.java
+++ week4-072.1.YourFirstAccount/src/Accounts.java
@@ -10,10 +10,19 @@ import java.util.Scanner;
 public class Accounts {
 
     public static void main(String[] args) {
+        /** Exercise 72.1 */
         Account account = new Account("Thang",100);
         account.deposit(20);
 
-        System.out.println(account);
+        /** Exercise 72.2 */
+        Account secondAccount = new Account("Matt's account",1000);
+        Account myAcc = new Account("My account", 0);
+        secondAccount.withdrawal(100.0);
+        myAcc.deposit(100.0);
+        System.out.println(secondAccount);
+        System.out.println(myAcc);
+
+
     }
 
 }
