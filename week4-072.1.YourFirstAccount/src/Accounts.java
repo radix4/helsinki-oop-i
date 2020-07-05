e589a08902786ac5484fe4b7ab78b33127c6f312
Thang Cao <shiyoline@gmail.com>
2020-07-05 12:19:46 -0700

    Exercise 72.2  completed.

--- week4-072.1.YourFirstAccount/src/Accounts.java
+++ week4-072.1.YourFirstAccount/src/Accounts.java
@@ -1,11 +1,19 @@
+/**
+ * This program
+ * @author  Thang Cao
+ * @since   07/05/2020
+ * */
+
 
 import java.util.Scanner;
 
 public class Accounts {
 
     public static void main(String[] args) {
-        // Code in Account.Java should not be touched!
-        // write your code here
+        Account account = new Account("Thang",100);
+        account.deposit(20);
+
+        System.out.println(account);
     }
 
 }
