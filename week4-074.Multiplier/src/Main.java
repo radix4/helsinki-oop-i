38f1065928d7f2b3ec05bcdd30a25ea487761618
Thang Cao <shiyoline@gmail.com>
2020-07-06 17:48:23 -0700

    Exercise 74 completed.

--- week4-074.Multiplier/src/Main.java
+++ week4-074.Multiplier/src/Main.java
@@ -1,7 +1,18 @@
-
+/**
+ * This program demonstrates class' method.
+ * @author  Thang Cao
+ * @since   07/06/2020
+ * */
 public class Main {
 
     public static void main(String[] args) {
-        // This is an empty main method which you can use to test your class's functionality.
+        Multiplier threeMultiplier = new Multiplier(3);
+        System.out.println("threeMultiplier.multiply(2): " + threeMultiplier.multiply(2));
+
+        Multiplier fourMultiplier = new Multiplier(4);
+        System.out.println("fourMultiplier.multiply(2): " + fourMultiplier.multiply(2));
+
+        System.out.println("threeMultiplier.multiply(1): " + threeMultiplier.multiply(1));
+        System.out.println("fourMultiplier.multiply(1): " + fourMultiplier.multiply(1));
     }
 }
