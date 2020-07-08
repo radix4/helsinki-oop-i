f6bb7aa0a7491debd5324ccad55d94c57c5938e3
Thang Cao <shiyoline@gmail.com>
2020-07-07 17:31:20 -0700

    Exercise 77.1 completed.

---
+++ week4-077.LyyraCard/src/LyyraCard.java
@@ -0,0 +1,11 @@
+public class LyyraCard {
+    private double balance;
+
+    public LyyraCard(double balanceAtStart) {
+        this.balance = balanceAtStart;
+    }
+
+    public String toString() {
+        return "The card has " + this.balance + " euros";
+    }
+}
--- week4-077.LyyraCard/src/Main.java
+++ week4-077.LyyraCard/src/Main.java
@@ -1,11 +1,15 @@
-
+/**
+ * This program simulates the Lyyra card.
+ * @author  Thang Cao
+ * @since   07/07/2020
+ * */
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // add here code that tests LyraCard. However before doing 77.6 remove the
-        // other code 
+        LyyraCard card = new LyyraCard(50);
+        System.out.println(card);
 
     }
 }
