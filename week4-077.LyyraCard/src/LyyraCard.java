20b1df38c76fe32f1c325e499db6451723e56d43
Thang Cao <shiyoline@gmail.com>
2020-07-07 17:32:23 -0700

    Exercise 77.2 completed.

--- week4-077.LyyraCard/src/LyyraCard.java
+++ week4-077.LyyraCard/src/LyyraCard.java
@@ -1,8 +1,22 @@
 public class LyyraCard {
     private double balance;
+    private final double ECONOMICAL = 2.5;
+    private final double GOURMET = 4.0;
 
     public LyyraCard(double balanceAtStart) {
         this.balance = balanceAtStart;
+    }
+
+    public void payEconomical() {
+        if (this.balance >= ECONOMICAL) {
+            this.balance -= ECONOMICAL;
+        }
+    }
+
+    public void payGourmet() {
+        if (this.balance >= GOURMET) {
+            this.balance -= GOURMET;
+        }
     }
 
     public String toString() {
