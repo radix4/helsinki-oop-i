f8a1a7f2493326d6bed3b5b86188d2f5f2b48ce8
Thang Cao <shiyoline@gmail.com>
2020-07-08 21:21:06 -0700

    Exercise 79.1 completed.

--- week4-079.NumberStatistics/src/Main.java
+++ week4-079.NumberStatistics/src/Main.java
@@ -1,16 +1,18 @@
+/**
+ * This program implements a couple of simple arithmetic functions.
+ * @author  Thang Cao
+ * @since   07/08/2020
+ * */
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        // Write test code here
-        // Remember to remove all the extra code when doing assignments  79.3 and 79.4
-
-        // Define three NumberStatistics objects in your program:
-        // The first is used to track the sum of all given numbers.
-        // The second takes care of even numbers and the third the odd numbers.
-
-        // The tests does not work if you do not create the objects in the correct order
-
+        NumberStatistics stats = new NumberStatistics();
+        stats.addNumber(3);
+        stats.addNumber(5);
+        stats.addNumber(1);
+        stats.addNumber(2);
+        System.out.println("Amount: " + stats.amountOfNumbers());
     }
 }
--- week4-079.NumberStatistics/src/NumberStatistics.java
+++ week4-079.NumberStatistics/src/NumberStatistics.java
@@ -1,3 +1,15 @@
-
 public class NumberStatistics {
+    private int amountOfNumbers;
+
+    public NumberStatistics() {
+        this.amountOfNumbers = 0;
+    }
+
+    public void addNumber(int number) {
+        this.amountOfNumbers++;
+    }
+
+    public int amountOfNumbers() {
+        return this.amountOfNumbers;
+    }
 }
