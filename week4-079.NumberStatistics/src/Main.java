b0bac321374caab704d8066939fc7365ff060806
Thang Cao <shiyoline@gmail.com>
2020-07-08 21:22:47 -0700

    Exercise 79 completed.

--- week4-079.NumberStatistics/src/Main.java
+++ week4-079.NumberStatistics/src/Main.java
@@ -8,11 +8,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        NumberStatistics stats = new NumberStatistics();
-        stats.addNumber(3);
-        stats.addNumber(5);
-        stats.addNumber(1);
-        stats.addNumber(2);
-        System.out.println("Amount: " + stats.amountOfNumbers());
+        Scanner reader = new Scanner(System.in);
+
+        NumberStatistics sum = new NumberStatistics();
+        NumberStatistics even = new NumberStatistics();
+        NumberStatistics odd = new NumberStatistics();
+
+        System.out.println("Type numbers: ");
+
+        while (true) {
+            int number = Integer.parseInt(reader.nextLine());
+            if (number == -1) {
+                break;
+            }
+
+            sum.addNumber(number);
+
+            if (number % 2 == 0) {
+                even.addNumber(number);
+            } else {
+                odd.addNumber(number);
+            }
+        }
+
+        System.out.println("sum: " + sum.sum());
+        System.out.println("sum of even: " + even.sum());
+        System.out.println("sum of odd: " + odd.sum());
     }
 }
