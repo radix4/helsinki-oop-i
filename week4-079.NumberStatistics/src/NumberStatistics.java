29d19ea9c73bd61b0fddbdb9b6caf5c32ef99b6e
Thang Cao <shiyoline@gmail.com>
2020-07-08 21:22:13 -0700

    Exercise 79.2 completed.

--- week4-079.NumberStatistics/src/NumberStatistics.java
+++ week4-079.NumberStatistics/src/NumberStatistics.java
@@ -1,8 +1,10 @@
 public class NumberStatistics {
     private int amountOfNumbers;
+    private int sum;
 
     public NumberStatistics() {
         this.amountOfNumbers = 0;
+        this.sum = 0;
     }
 
     public void addNumber(int number) {
@@ -11,5 +13,17 @@ public class NumberStatistics {
 
     public int amountOfNumbers() {
         return this.amountOfNumbers;
+    }
+
+    public int sum() {
+        return this.sum;
+    }
+
+    public double average() {
+        if (this.amountOfNumbers == 0) {
+            return 0;
+        }
+
+        return 1.0 * this.sum / this.amountOfNumbers;
     }
 }
