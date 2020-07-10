af6b5ec9fef82de56c1144fad0ba83d917820b5e
Thang Cao <shiyoline@gmail.com>
2020-07-09 17:42:38 -0700

    Exercise 81 completed.

--- week4-081.PasswordRandomizer/src/PasswordRandomizer.java
+++ week4-081.PasswordRandomizer/src/PasswordRandomizer.java
@@ -2,13 +2,26 @@ import java.util.Random;
 
 public class PasswordRandomizer {
     // Define the variables
+    private Random random = new Random();
+    private int length;
 
     public PasswordRandomizer(int length) {
-        // Initialize the variable
+        this.length = length;
     }
 
     public String createPassword() {
-        // write code that returns a randomized password
-        return "";
+        String characters = "abcdefghijklmnopqrstuvwxyz";
+
+        StringBuilder password = new StringBuilder("");
+        int numberOfCharacters = this.length;
+
+        while (numberOfCharacters > 0) {
+            int random = this.random.nextInt(characters.length());
+            char c = characters.charAt(random);
+            password.append(c);
+            numberOfCharacters--;
+        }
+
+        return password.toString();
     }
 }
--- week4-081.PasswordRandomizer/src/Program.java
+++ week4-081.PasswordRandomizer/src/Program.java
@@ -1,3 +1,9 @@
+/**
+ * This program generates a randomized password.
+ * @author  Thang Cao
+ * @since   07/09/2020
+ * */
+
 public class Program {
     public static void main(String[] args) {
         PasswordRandomizer randomizer = new PasswordRandomizer(13);
