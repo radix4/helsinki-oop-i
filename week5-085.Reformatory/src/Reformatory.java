ed24f3c346dc67829411d86d3317b63992a65dca
Thang Cao <shiyoline@gmail.com>
2020-07-11 18:59:10 -0700

    Exercise 85.2 cmopleted.

--- week5-085.Reformatory/src/Reformatory.java
+++ week5-085.Reformatory/src/Reformatory.java
@@ -3,5 +3,8 @@ public class Reformatory {
     public int weight(Person person) {
         return person.getWeight();
     }
-
+    public void feed(Person person) {
+        int weightBeforeEating = person.getWeight();
+        person.setWeight(weightBeforeEating + 1);
+    }
 }
