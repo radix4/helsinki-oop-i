9e75560a104a010595ca80bafc2a0aacd09821bd
Thang Cao <shiyoline@gmail.com>
2020-07-13 22:01:57 -0700

    Exercise 88.1 completd.

--- week5-088.Students/src/Main.java
+++ week5-088.Students/src/Main.java
@@ -1,3 +1,8 @@
+/**
+ * This program creates a list of students then find them using keywords.
+ * @author  Thang Cao
+ * @since   07/13/2020
+ * */
 
 import java.util.ArrayList;
 import java.util.Scanner;
