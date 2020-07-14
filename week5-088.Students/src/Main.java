c7ea9fd88e27a517bb7e692309c7f93800fc1760
Thang Cao <shiyoline@gmail.com>
2020-07-13 22:02:35 -0700

    Exercise 88 completed.

--- week5-088.Students/src/Main.java
+++ week5-088.Students/src/Main.java
@@ -11,5 +11,33 @@ public class Main {
 
     public static void main(String[] args) {
         // write here the main program
+        ArrayList<Student> list = new ArrayList<Student>();
+        Scanner reader = new Scanner(System.in);
+        while (true) {
+            System.out.print("name: ");
+            String name = reader.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+            System.out.print("student number: ");
+            String number = reader.nextLine();
+            list.add(new Student(name, number));
+        }
+
+        for (Student student : list) {
+            System.out.println(student);
+        }
+
+        System.out.println();
+
+        System.out.print("Give search term: ");
+        String searchTerm = reader.nextLine();
+        System.out.println("Result:");
+
+        for (Student student : list) {
+            if (student.getName().contains(searchTerm)) {
+                System.out.println(student);
+            }
+        }
     }
 }
