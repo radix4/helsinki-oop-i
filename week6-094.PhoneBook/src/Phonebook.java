b308f9b85b5cfb0022daa513f63e2bdfe770a128
Thang Cao <shiyoline@gmail.com>
2020-07-16 18:26:10 -0700

    Exercise 94 completed.

---
+++ week6-094.PhoneBook/src/Phonebook.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Phonebook {
+
+    private ArrayList<Person> list;
+
+    public Phonebook() {
+        list = new ArrayList<Person>();
+    }
+
+    public void add(String name, String number) {
+        list.add(new Person(name, number));
+    }
+
+    public void printAll() {
+        for (Person person : list) {
+            System.out.println(person);
+        }
+    }
+
+    public String searchNumber(String name) {
+        for (Person person : list) {
+            if (person.getName().equals(name)) {
+                return person.getNumber();
+            }
+        }
+
+        return "number not known";
+    }
+}
