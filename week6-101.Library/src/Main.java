e391bc191ba7ec8ad124c7fdc0b01d479718fe76
Thang Cao <shiyoline@gmail.com>
2020-07-19 18:20:59 -0700

    Added class Book.

--- week6-101.Library/src/Main.java
+++ week6-101.Library/src/Main.java
@@ -1,5 +1,16 @@
+/**
+ * This program implements a simple information system prototype for a library.
+ * @author  Thang Cao
+ * @since   07/19/2020
+ * */
+
 public class Main {
     public static void main(String[] args) {
-        // test your program here
+        Book cheese = new Book("Cheese Problems Solved", "Woodhead Publishing", 2007);
+        System.out.println(cheese.title());
+        System.out.println(cheese.publisher());
+        System.out.println(cheese.year());
+
+        System.out.println(cheese);
     }
 }
