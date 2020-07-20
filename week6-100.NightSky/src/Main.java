344280c0f689daf7a87ee9a75d809993cfc415d0
Thang Cao <shiyoline@gmail.com>
2020-07-19 18:16:53 -0700

    Exercise 100 completed.

--- week6-100.NightSky/src/Main.java
+++ week6-100.NightSky/src/Main.java
@@ -1,7 +1,18 @@
+/**
+ * This program stimulates a night sky with stars by printing.
+ * @author  Thang Cao
+ * @since   07/19/2020
+ * */
 
 public class Main {
 
     public static void main(String[] args) {
-        // Test your code here
+        NightSky NightSky = new NightSky(8, 4);
+        NightSky.print();
+        System.out.println("Number of stars: " + NightSky.starsInLastPrint());
+        System.out.println("");
+
+        NightSky.print();
+        System.out.println("Number of stars: " + NightSky.starsInLastPrint());
     }
 }
