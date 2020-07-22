00af165bb74a334c52c54aa6606a49cafda807bd
Thang Cao <shiyoline@gmail.com>
2020-07-21 18:39:45 -0700

    Exercise 104.1 completed.

--- week6-104.Sorting/src/Main.java
+++ week6-104.Sorting/src/Main.java
@@ -1,7 +1,24 @@
+/**
+ * This program sorts an array.
+ * @author  Thang Cao
+ * @since   07/20/2020
+ * */
 
 public class Main {
     public static void main(String[] args) {
-        // write testcode here
+        int[] values = {6, 5, 8, 7, 11};
+        System.out.println("smallest: " + smallest(values));
+    }
+
+    public static int smallest(int[] array) {
+        int smallest = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < smallest) {
+                smallest = array[i];
+            }
+        }
+        return smallest;
     }
 
 }
+
