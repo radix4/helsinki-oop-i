14008d7d9b2ee973e70fed8bdc27fc34d60ef9db
Thang Cao <shiyoline@gmail.com>
2020-07-21 18:43:46 -0700

    Exercise 105 completed.

--- week6-105.GuessingGame/src/Main.java
+++ week6-105.GuessingGame/src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         // test your program here
         GuessingGame game = new GuessingGame();
+
+        game.instructions(1,10);
         game.play(1,10);
     }    
 }
